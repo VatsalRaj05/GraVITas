ba7da2404faa8d895f83867457b9b4b69fa60b20
Vatsalraj05 <vatsalraj2005@gmail.com>
2023-09-22 14:32:10 +0530

    SyncTest1.py

--- SyncTest1.py
+++
@@ -1,9 +0,0 @@
-print("I love you Vatsal")
-a = input("Do you love me? Please enter as Y/N")
-
-if(a == "Y"):
-    print("Yay! I am your Boyfriend")
-elif(a == "N"):
-    print("I thot u are gay")
-else:
-    print("Tu hai hi Chutiya")
